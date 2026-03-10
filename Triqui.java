abcd636f57d3346dffe9be72864ebcc0a66a106e
mikeystc <tabordaceronmichael@gmail.com>
2026-03-10 17:24:31 -0500

    Add files via upload

---
+++ Triqui.java
@@ -0,0 +1,17 @@
+#BlueJ class context
+comment0.target=Triqui
+comment1.params=
+comment1.target=Triqui()
+comment2.params=
+comment2.target=void\ printBoard()
+comment3.params=row\ col
+comment3.target=boolean\ makeMove(int,\ int)
+comment4.params=
+comment4.target=boolean\ checkWin()
+comment5.params=
+comment5.target=boolean\ checkDraw()
+comment6.params=
+comment6.target=void\ switchPlayer()
+comment7.params=args
+comment7.target=void\ main(java.lang.String[])
+numComments=8
